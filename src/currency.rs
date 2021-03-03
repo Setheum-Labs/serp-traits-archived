7703c9956c0c879cfb081419f4e969ef33fb63fa
Muhammad-Jibril <jbashir52@gmail.com>
2021-03-04 00:39:56 +0800

    deposit_expand_issuance,withdraw_contract_issuance

--- src/currency.rs
+++ src/currency.rs
@@ -27,20 +27,13 @@ pub trait SettCurrency<AccountId> {
 	/// Existential deposit of `currency_id`.
 	fn minimum_balance(currency_id: Self::CurrencyId) -> Self::Balance;
 
-	/// Reduce the total issuance of SettCurrency by `amount` and return the according imbalance. The imbalance will
-	/// typically be used to reduce an account by the same amount with e.g. `settle`.
-	///
-	/// This is infallible, but doesn't guarantee that the entire `amount` is burnt, for example
-	/// in the case of underflow.
-	fn contract_issuance(currency_id: Self::CurrencyId, amount: Self::Balance) -> DispatchResult;
+	/// Add `amount` to the balance of `who` under `currency_id` and increase
+	/// total issuance.
+	fn deposit_expand_issuance(currency_id: Self::CurrencyId, who: &AccountId, amount: Self::Balance) -> DispatchResult;
 
-	/// Increase the total issuance of SettCurrency by `amount` and return the according imbalance. The imbalance
-	/// will typically be used to increase an account by the same amount with e.g.
-	/// `resolve_into_existing` or `resolve_creating`.
-	///
-	/// This is infallible, but doesn't guarantee that the entire `amount` is issued, for example
-	/// in the case of overflow.
-	fn expand_issuance(currency_id: Self::CurrencyId, amount: Self::Balance) -> DispatchResult;
+	/// Remove `amount` from the balance of `who` under `currency_id` and reduce
+	/// total issuance.
+	fn withdraw_contract_issuance(currency_id: Self::CurrencyId, who: &AccountId, amount: Self::Balance) -> DispatchResult;
 
 	/// The total amount of issuance of `currency_id`.
 	fn total_issuance(currency_id: Self::CurrencyId) -> Self::TotalIssuance;
